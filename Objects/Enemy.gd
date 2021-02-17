af25e4d9997da336e2960e234f63b3d27b37a913
realyurimednikov <mednikov.iurii@gmail.com>
2021-02-17 10:11:09 +0100

    Added a simple enemy behavior
    Implemented a simple enemy behavior to detect a player and to look to him

--- Objects/Enemy.gd
+++ Objects/Enemy.gd
@@ -1,11 +1,43 @@
 extends KinematicBody
 
 
-var health = 100
+onready var sphere = $MeshInstance
 
+
+var health = 100
+var target
 
 func damage(x):
 	if health > 0:
 		health -= x
 	else:
 		queue_free()
+		
+
+func _process(delta):
+	if target:
+		look_at_player()
+
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("Player"):
+		target = body
+		set_to_red()
+
+
+func _on_Area_body_exited(body):
+	if body.is_in_group("Player"):
+		target = null
+		set_to_green()
+	
+
+func set_to_green():
+	sphere.get_surface_material(0).set_albedo(Color(0, 1, 0))
+
+
+func set_to_red():
+	sphere.get_surface_material(0).set_albedo(Color(1, 0, 0))
+
+
+func look_at_player():
+	look_at(target.global_transform.origin, Vector3.UP)
