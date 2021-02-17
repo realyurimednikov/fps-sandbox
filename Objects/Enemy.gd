3c76394003da6994bcd1ad4b6d46735807fe8b42
realyurimednikov <mednikov.iurii@gmail.com>
2021-02-17 10:28:12 +0100

    Made an enemy to chase a player

--- Objects/Enemy.gd
+++ Objects/Enemy.gd
@@ -1,11 +1,19 @@
 extends KinematicBody
 
 
+export var speed = 15
+
+
 onready var sphere = $MeshInstance
 
 
 var health = 100
 var target
+var space_state
+
+
+func _ready():
+	space_state = get_world().direct_space_state
 
 func damage(x):
 	if health > 0:
@@ -16,7 +24,14 @@ func damage(x):
 
 func _process(delta):
 	if target:
-		look_at_player()
+#		look_at_player()
+		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
+		var collider = result.collider
+		if collider.is_in_group("Player"):
+			look_at_player()
+			follow_player(delta)
+		else:
+			set_to_green()
 
 
 func _on_Area_body_entered(body):
@@ -41,3 +56,8 @@ func set_to_red():
 
 func look_at_player():
 	look_at(target.global_transform.origin, Vector3.UP)
+	
+
+func follow_player(delta):
+	var direction = (target.transform.origin - transform.origin).normalized()
+	move_and_slide(direction * speed * delta, Vector3.UP)
