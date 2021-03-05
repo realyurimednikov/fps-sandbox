acb3aa9a6a3001c0ad438a5187086f6725789fd9
realyurimednikov <mednikov.iurii@gmail.com>
2021-03-05 12:54:29 +0100

    Changed Weapon script
    1. Moved input handling for fire() and reload() from Weapon.gd to Player.gd
    2. Weapon class extends Spatial class
    3. Created is_possible_shoot varialble. Caller can use the variable to update UI
    4. Added do_on_shoot() and do_on_reload() callbacks to Weapon.gd
    5. Animation was moved to concrete implementations and can be called with callbacks (see p.4)
    6. Helper methods enable_gun() and disable_gun() allows to encapsulate enable/disable logic for the weapon
    7. Created active_weapon field in Player.gd
    8. Refactored update_ui() in Player.gd

--- Player/AssaultRifleGun.gd
+++ Player/AssaultRifleGun.gd
@@ -2,3 +2,9 @@ extends Weapon
 
 class_name AssautRifleGun
 
+
+onready var animation_player =  $AnimationPlayer
+
+
+func do_on_shoot():
+	animation_player.play("FireAnimation")
--- Player/Weapon.gd
+++ Player/Weapon.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Spatial
 
 class_name Weapon
 
@@ -10,14 +10,16 @@ export var damage_value = 25
 export var is_active = false
 export var shoot_speed = 5
 export var fire_distance = -30
+export var weapon_name = ""
 
 var current_ammo = 0 
 var can_fire = true
 var reloading = false
+var is_possible_shoot = false
 
 onready var raycast = $"./RayCast"
 onready var muzzle = $"./Muzzle"
-onready var animation_player = $"./AnimationPlayer"
+#onready var animation_player = $"./AnimationPlayer"
 onready var bullet = preload("res://Player/Bullet.tscn")
 
 func update_ui():
@@ -27,12 +29,12 @@ func update_ui():
 func _ready():
 	current_ammo = clip_size
 	raycast.cast_to.z = fire_distance
-	if is_active:
-		update_ui()
+#	if is_active:
+#		update_ui()
 	
 
 func fire():
-	if raycast.is_colliding():
+	if is_possible_shoot:
 		var b = bullet.instance()
 		muzzle.add_child(b)
 		b.look_at(raycast.get_collision_point(), Vector3.UP)
@@ -41,45 +43,75 @@ func fire():
 		b.shoot = true
 
 func reload():
-	animation_player.stop()
+#	animation_player.stop()
+	do_on_reload()
 	reloading = true
 	yield(get_tree().create_timer(reload_rate), "timeout")
 	current_ammo = clip_size
 	reloading = false
-	update_ui()
+#	update_ui()
 
 
-func _process(delta):
-	if is_active:
-#		if raycast.is_colliding():
-#			can_fire = true
-#		else:
-#			can_fire = false
-		if Input.is_action_pressed("reload") and not reloading:
+func shoot():
+	if can_fire:
+		if current_ammo > 0 and not reloading:
+			can_fire = false
+			current_ammo -= 1
+#			fire
+			fire()
+			do_on_shoot()
+			yield(get_tree().create_timer(fire_rate), "timeout")
+			can_fire = true
+		elif not reloading:
 			reload()
-		if Input.is_action_just_pressed("primary_fire") and can_fire:
-			if current_ammo > 0 and not reloading:
-				
-				can_fire = false
-				current_ammo -= 1
-				if not animation_player.is_playing():
-					fire()
-				animation_player.play("FireAnimation")
-				yield(get_tree().create_timer(fire_rate), "timeout")
-				can_fire = true
-				
-			elif not reloading:
-				reload()
+			
+func do_on_shoot():
+	pass
 
-			else:
-				animation_player.stop()
+
+func do_on_reload():
+	pass
+
+func _physics_process(delta):
+	if is_active:
+		if raycast.is_colliding():
+			is_possible_shoot = true
+	else:
+		is_possible_shoot = false
+
+#func _process(delta):
+#	if is_active:
+##		if raycast.is_colliding():
+##			can_fire = true
+##		else:
+##			can_fire = false
+#		if Input.is_action_pressed("reload") and not reloading:
+#			reload()
+#		if Input.is_action_just_pressed("primary_fire") and can_fire:
+#			if current_ammo > 0 and not reloading:
+#
+#				can_fire = false
+#				current_ammo -= 1
+#				if not animation_player.is_playing():
+#					fire()
+#				animation_player.play("FireAnimation")
+#				yield(get_tree().create_timer(fire_rate), "timeout")
+#				can_fire = true
+#
+#			elif not reloading:
+#				reload()
+#
+#			else:
+#				animation_player.stop()
 				
-			update_ui()		
+#		update_ui()		
 
 
 func enable_gun():
 	is_active = true
-	update_ui()
+	visible = true
+#	update_ui()
 
 func disable_gun():
 	is_active = false
+	visible = false
